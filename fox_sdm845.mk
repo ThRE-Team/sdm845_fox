40d9d2658da4f752c6a6a5d5e6d15d03c555c48d
Darth9 <reyprimerjefe@gmail.com>
2025-01-31 17:53:25 +0000

    Build all the partition tools

--- fox_sdm845.mk
+++ fox_sdm845.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022-2024 The OrangeFox Recovery Project
+# Copyright (C) 2022-2025 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
@@ -59,6 +59,9 @@ OF_DONT_KEEP_LOG_HISTORY := 1
 
 # dynamic partitions?
 ifeq ($(FOX_USE_DYNAMIC_PARTITIONS),1)
+   # build all the partition tools
+   OF_ENABLE_ALL_PARTITION_TOOLS := 1
+   #
    OF_QUICK_BACKUP_LIST := /boot;/data;
    OF_DISABLE_OTA_MENU := 1
    OF_NO_ADDITIONAL_MIUI_PROPS_CHECK := 1
