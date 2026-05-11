a5592521acc808577ba11bf20e6fa603ed6b728f
ThRE Team <threteam@gmail.com>
2026-05-11 19:55:00 +0700

    OrangeFox Recovery for beryllium
    - Sources:
      * https://gitlab.com/OrangeFox/device/beryllium
      * https://gitlab.com/OrangeFox/device/dipper
      * https://github.com/kinguser981/OrangeFox-Recovery-Builder-2024
      * https://github.com/duckyduckG/android_kernel_xiaomi_sdm845_419
      * https://github.com/crdroidandroid/android_kernel_xiaomi_sdm845

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2022-2024 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
+#
+
+# Inherit the common sdm845 BoardConfig
+include device/xiaomi/sdm845-common/BoardConfigCommon.mk
+
+# Decrypt A16
+ifeq ($(FOX_KERNEL),4.19)
+    BOARD_USES_QCOM_FBE_DECRYPTION := true
+    BOARD_USES_QCOM_ICE := true
+    TW_INCLUDE_CRYPTO := true
+    TW_INCLUDE_FBE_METADATA_DECRYPT := true
+    BOARD_USES_METADATA_PARTITION := true
+endif
